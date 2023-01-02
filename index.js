d8c8f1b8a795d701cc11f60eed63b6d62ed44945
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2023-01-02 19:22:37 +0530

    user signup form created

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("this is working right now");
+//design a signup form
+
+const uid = "abc1234"
+
+var fullName = "Abdul Ahad"
+var email = "abdul@gmail.com"
+var password = "123456"
+var confirmPassword = "123456"
+var couseCount = 0
+var isLoggedInFromGoogle = false
+
+//take input from the user
+//you can take input using 'prompt'
+
+//fullName = prompt("enter your name")
+
+console.log(`
+    with unique id: ${uid}
+    user is: ${fullName}
+    password is: ${password}
+`)
