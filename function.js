1eabbdf1b0f1fe1cb92fceaa1dbb2f212da87a41
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2023-01-03 00:01:02 +0530

    new stuff added

--- function.js
+++ function.js
@@ -12,4 +12,12 @@ function namasteUser(user){
     return `namaste ${user}`
 }
 
-console.log(namasteUser("ahad"))
+// console.log(namasteUser("ahad"))
+
+//you can also write the above function body as follows
+
+var namasteUser = function(user){
+    return `namaste ${user} from second function`
+}
+
+console.log(namasteUser("asad"))
