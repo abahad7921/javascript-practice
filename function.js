5d40dd5790988e54c70af59521efde667563e460
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2023-01-02 23:54:31 +0530

    functions file added

---
+++ function.js
@@ -0,0 +1,15 @@
+//basic function declaration
+
+function greetUser(user){
+    console.log(`good evening ${user}`)
+}
+
+greetUser("ahad")
+
+//how to return from functions?
+
+function namasteUser(user){
+    return `namaste ${user}`
+}
+
+console.log(namasteUser("ahad"))
