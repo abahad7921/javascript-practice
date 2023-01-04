99800936e6d5f9ea5969e9d9a46800ea48e1e1a2
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2023-01-04 12:51:22 +0530

    more examples added

--- hoisting.js
+++ hoisting.js
@@ -11,7 +11,7 @@ function tipper(a){
 
 tipper("5") //this works as usual
 
-bigTipper("55") //this will not work as this is variable
+// bigTipper("55") //this will not work as this is variable
 
 var bigTipper = function(a){
     var bill = parseInt(a);
@@ -19,3 +19,21 @@ var bigTipper = function(a){
 }
 
 // bigTipper("55")  will work fine
+
+var name = "Abdul Ahad"
+
+function sayName(){
+    var name = "MD Asad"
+    console.log(name)
+}
+
+sayName()
+
+console.log(name)
+
+/**
+ in this example the second execution context window loads up for sayName() function
+ in the second context name is overriden to MD asad
+ and when the function call ends then the execution context loads off
+ and the name is undo to Abdul Ahad
+ */
