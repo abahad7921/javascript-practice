96c7a5e4dc233b2e127613375ffb6a997ce6620b
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2023-01-04 11:59:49 +0530

    context example in global scope added

--- context.js
+++ context.js
@@ -4,3 +4,24 @@ function sayHello(){
     console.log("hello");
 }
 
+//here you can see context of the function sayHello
+//the above statement will run fine as usual
+
+if(2 === 2){
+    console.log("this is true")
+}
+
+var myName = "Abdul Ahad"
+
+if(myName === myName){
+    console.log("this is a true statement")
+}
+
+//this will work as usual
+
+if(myName === window.myName){
+    console.log("this is again a true statement")
+}
+
+//this will produce an error as the current context is node not the global context
+
